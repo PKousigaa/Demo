c2f0d4c146f457ac2d09c8d28167ce8f4a9dd521
PKousigaa <kousigaa.pandiyan@gmail.com>
2024-11-25 19:55:15 +0530

    messege

---
+++ new.js
@@ -0,0 +1,64 @@
+
+
+/*var price = 78
+var product= "iphone10"
+
+var tax= 10
+     
+console.log(product)
+console.log(price+tax)*/
+
+var a="apple"
+console.log(a)
+var FruitName="Mango"
+var count=100
+var price=20
+var total=(count*price)
+console.log(FruitName)
+console.log(total)
+let b=99
+let c=++b
+console.log(c)
+var d=88
+var e=++d
+console.log(e)
+function abc()
+{
+    console.log("hello")
+}
+abc()
+function myname()
+{
+    console.log("kousi")
+}
+myname()
+var a=90
+var h=9
+function add()
+    {console.log(a+h)
+
+    }
+add()
+  var fmovie= "three"
+  var fline="all is well"
+  function Favourite()
+  {console.log("Favourite movie:"+fmovie)
+  console.log("Favourite line:"+fline)
+  }
+  Favourite()
+  Favourite()
+  Favourite()
+function area(l,b)
+{   var j=l*b
+    console.log("Area is:"+j)
+
+}
+area(19,45)
+
+function myname()
+{ return "pk"
+
+}
+var n=myname()
+console.log(n)
+
